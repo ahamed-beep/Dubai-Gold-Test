707aed65e475bcdd94be53a22a236e5a7666a376
ahamed-beep <muhamedahamed251@gmail.com>
2025-09-22 04:08:32 -0700

    your commit message

--- src/Components/Verifygold.jsx
+++ src/Components/Verifygold.jsx
@@ -61,7 +61,7 @@ const Verifygold = () => {
         }}
       >
         {/* Optional overlay for better text readability */}
-        <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+        <div className="absolute inset-0  bg-opacity-40"></div>
       </div>
       
       {/* Content Container */}
