56e05f9422ff18d4947a2a4f5fe90ac66e1aa6fd
ahamed-beep <muhamedahamed251@gmail.com>
2025-09-22 06:50:14 -0700

    your commit message

--- src/Components/Verifygold.jsx
+++ src/Components/Verifygold.jsx
@@ -128,7 +128,7 @@ const Verifygold = () => {
 
       {/* Logo - Absolute Bottom Right (only in this section) */}
       <div className="absolute bottom-5 right-5 z-30">
-        <div className="md:w-25 md:h-25 w-20 h-20  text-white flex items-center justify-center ">
+        <div className="md:w-25 md:h-25 w-16 h-16  text-white flex items-center justify-center ">
           <img src="/Images/stamp.png"  />
         </div>
       </div>
